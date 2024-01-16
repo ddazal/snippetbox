a0f28896a1c258f0f513d7baafb2b6447e2bce32
David Daza <03dazal@gmail.com>
2024-01-16 05:26:10 -0500

    serve static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir(filepath.Join("ui", "static")))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
